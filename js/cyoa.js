2ab944d91cd1b306a8f5d58d344b40065bdc9b58
Ashley Zimmerman <ashsmith7@yahoo.com>
2018-04-26 18:53:01 -0400

    update js files

---
+++ js/cyoa.js
@@ -0,0 +1,48 @@
+//var = prompt("You are driving down a road and need to backup to leave and you get stuck. Type 'bump' for the car getting stuck, type 'open door' to get out of the car");
+var "bump" = prompt("the car gets stuck");
+var "open door" = prompt("you get out of the car");
+if(bump && open door){
+  output.innerHTML = "<p>the car gets stuck,you get out of car, " + bump + " " + open door + ".</p>";
+} else {
+  output.innerHTML = "<p>you get out of the car!</p>";
+}
+
+} else {
+    alert("see that you are in a ditch and someone coming down the road");
+
+
+
+var = prompt("Someone is coming down the road. type 'look left' to see someone driving down the road. type 'happy' that they stop to and pull you out");
+
+}
+if(response === "look left"){
+  response = promote("someone is driving down the road")
+} else if (response ==="happy") {
+  response = prompt("they stop and pull you out");
+} else {
+  alert("glad they are helpful, but if not helpful you are scared");
+}
+var response = prompt("You leave the car. Type 'walk' when you start walking down the long dark dirt road. Type 'shocked' when you see a scary abandon house");
+
+}
+if(response === "walking"){
+  response = promote("leave car and start walking down the road")
+} else if (response ==="shocked") {
+  response = prompt("when you see a scary abandon house");
+} else {
+  alert("you are starting to run away, either into house or back to car?");
+}
+
+var response = prompt("You notice something next to the house that scares you. Type 'look left', then you run into the house. Type'run'.");
+
+}
+if(response === "look left"){
+  response = promote("you notice something next to the house that scares you")
+} else if (response ==="run") {
+  response = prompt("you run into the house");
+} else {
+  alert("you are not sure you did the right thing, did you?");
+}
+
+
+}
--- js/greeter.js
+++ js/greeter.js
@@ -6,4 +6,20 @@ var checkName = function() {
     name = prompt('What is your name, for real?')
     checkName(); // repeats until a name exists
   }
+  var firstName = prompt("Hi there! What's your first name?");
+  var lastName = prompt("What's your last name?");
+  var output = document.querySelector('#greeting');
+
 }
+var checkName = function (){
+if(firstName && lastName){
+  output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
+} else {
+
+firstName = prompt('What is your first name?');
+lastName = prompt("What is your last name?");
+
+}
+//checkName(); //kicks off evaluation
+// alert(Hello, ' + Name);
+//output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
